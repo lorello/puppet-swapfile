c5a442d279bca9e95957224ec5111f1be0192a38
lorello <lorenzo.salvadorini@softecspa.it>
2016-03-03 13:06:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# swapfile
+
+#### Table of Contents
+
+1. [Description](#description)
+1. [Setup - The basics of getting started with swapfile](#setup)
+    * [What swapfile affects](#what-swapfile-affects)
+    * [Setup requirements](#setup-requirements)
+    * [Beginning with swapfile](#beginning-with-swapfile)
+1. [Usage - Configuration options and additional functionality](#usage)
+1. [Limitations - OS compatibility, etc.](#limitations)
+1. [Development - Guide for contributing to the module](#development)
+
+## Description
+
+Install and configure dphys-swapfile package to manage swap space on file instead of a dedicated disk partition
+
+## Setup
+
+### What swapfile affects
+
+The module manage the package dphys-swapfile and the config file
+`/etc/dphys-swapfile`
+
+### Beginning with swapfile
+
+Simply include the class
+
+## Usage
+
+This section is where you describe how to customize, configure, and do the
+fancy stuff with your module here. It's especially helpful if you include usage
+examples and code samples for doing things with your module.
+
+## Limitations
+
+This module is tested only on Ubuntu 10.04, 12.04 and 14.04, but I think
+it should run on any Debian based OS without problems.
+
+## Development
+
+Open an issue or fork and submit a PR if you find a bug or have something
+to suggests
+
+## Release Notes/Contributors/Etc. **Optional**
+
+| Release | Notes
+| ------- | ------------
+| 0.1.0   | First release
---
+++ manifests/init.pp
@@ -0,0 +1,98 @@
+# Class: swapfile
+# ===========================
+#
+# Install and configure dphys-swapfile package to manage swap space on file
+# instead of a dedicated disk partition
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `ensure`
+#  present or absent, manage package and config file presence (default: present)
+# * `file_path`
+#  full path of the file used for memory swap (default: /var/swap)
+# * `size`
+#  size in Mb of the swap file (default: 2 * memorysize)
+#
+# Examples
+# --------
+#
+# Create a 4G swapfile on /mnt path, typically used on EC2 hosts, where
+# `/mnt` is mounted on ephymeral storage.
+#
+# @example
+#    class { 'swapfile':
+#      file_path => '/mnt/swapfile',
+#      size      => 4096,
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <lorenzo.salvadorini@softecspa.it>
+#
+# Copyright
+# ---------
+#
+# Copyright 2016 Lorenzo Salvadorini, unless otherwise noted.
+#
+class swapfile(
+  $ensure    = present,
+  $file_path = '/var/swap',
+  $size      = undef,
+) {
+
+  validate_re($ensure, '(present|absent)', "ensure must be 'present' or 'absent', checked value is '${ensure}'")
+  validate_integer($size)
+
+  if ! is_absolute_path($file_path) {
+    fail("file_path must a valid absolute path, checked value is '${file_path}'")
+  }
+
+  case $::osfamily {
+    'Debian': {
+      case $::lsbdistid {
+        'Ubuntu': {
+          if $::lsbmajdistrelease < 12 {
+            fail('Ubuntu before Precise is not supported (missing package)')
+          }
+        }
+        'Debian': {
+          if $::lsbmajdistrelease < 7 {
+            fail('Debian before Wheezy is not supported (missing package)')
+          }
+        }
+        default: {
+          warn("Your OS distribution is unknown (value: ${::lsbdistid}), be sure the package dphys-swapfile is available in your repositories")
+        }
+      }
+    }
+    default: {
+      fail("This module is not supported on OS family '${::osfamily}'")
+    }
+  }
+
+  if $size == undef {
+    $real_size = 2 * $::memorysize_mb
+  } else {
+    $real_size = $size
+  }
+
+  file { '/etc/dphys-swapfile':
+    ensure  => $ensure,
+    content => "CONF_SWAPFILE=${file_path}\nCONF_SWAPSIZE=${real_size}\n",
+  }
+
+  package { 'dphys-swapfile':
+    ensure => $ensure,
+  }
+
+  if $ensure == present {
+    service { 'dphys-swapfile':
+      ensure => running,
+      enable => true,
+    }
+  }
+}
